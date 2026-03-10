8a88aba2753acaa89f0a338e0871f48fef62ac8a
sara <saraata003@gmail.com>
2026-03-11 00:41:04 +0300

    fix api url

--- frontend/app/card/[phone]/ui.tsx
+++ frontend/app/card/[phone]/ui.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { FaPaw, FaGift } from "react-icons/fa";
 import QRCode from "qrcode";
 
-const API = "http://127.0.0.1:8000";
+const API = process.env.NEXT_PUBLIC_API_URL!;
 
 type Customer = {
   full_name: string;
